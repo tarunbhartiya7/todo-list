4537c2d821bd077915cd395ad2c1ae21b9ebb7bf
Tarun Bhartiya <tarunbhartiya@Taruns-MacBook-Pro.local>
2019-03-04 01:23:37 +0530

    first commit

---
+++ src/assets/todos.js
@@ -0,0 +1,19 @@
+import uuid from 'uuid';
+
+export default [
+  {
+    id: uuid.v4(),
+    title: 'Learn ES6',
+    completed: false
+  },
+  {
+    id: uuid.v4(),
+    title: 'Watch movie',
+    completed: true
+  },
+  {
+    id: uuid.v4(),
+    title: 'Read Books',
+    completed: false
+  }
+]
---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>About</h1>
+        <p>This is the TodoList app v1.0.0. It is part of a React crash course</p>
+      </React.Fragment>
+    )
+  }
+}
